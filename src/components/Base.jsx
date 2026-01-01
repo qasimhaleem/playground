56caf450e5a78a17ddff43ba6e7e9d8ef0192cc0
Qasim Haleem <mrqasimhaleem@gmail.com>
2026-01-01 08:59:50 -0800

    file connected

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,4 +1,4 @@
-import App from "../App"
+import App from "./App"
 import Footer from "./footer"
 const Grand = () => {
     <App />
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/App.jsx'
+import Base from './components/Base.jsx'
 import Grand from './components/Base.jsx'
 import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-    <App />
+    <Grand />
   </StrictMode>
 )
