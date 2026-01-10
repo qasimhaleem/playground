ffbe4eb1e3a8d846b8899bf2a56033f3e9d27efd
Qasim Haleem <mrqasimhaleem@gmail.com>
2026-01-10 02:05:48 -0800

    Experince card is desined succesfully

---
+++ src/components/ExperincesCard.jsx
@@ -0,0 +1,22 @@
+const Cards = () => {
+    return (
+        <div className="bg-yellow-200 flex justify-center items-center mt-10 mx-4 p-30 gap-19">
+            <div className="border border-blac-400 rounded-lg ">
+                <strong className="mx-2">Web Expert </strong>
+                <p className="px-5">Lorem ipsum dolor sit amet consectetur<br /> adipisicing  elit. Deserunt nemo, <br /> delectus voluptas quod <br /> omnis, repellat <br />repudiandae ullam asperiores earum <br />sit sint commodi non minus. <br />Fuga necessitatibus nihil sunt odio illo.</p>
+                <button className="bg-blue-800 flex justify-center  items-center font-medium text-white mx-32 border-s-fuchsia-300 rounded-md shadow my-3 px-5 py-2 whitespace-nowrap w-ma">See More</button>
+            </div>
+            <div className="border border-blac-400 rounded-lg ">
+                <strong className="mx-2">Digital mareting Expert </strong>
+                <p className="px-5">Lorem ipsum dolor sit amet consectetur<br /> adipisicing  elit. Deserunt nemo, <br /> delectus voluptas quod <br /> omnis, repellat <br />repudiandae ullam asperiores earum <br />sit sint commodi non minus. <br />Fuga necessitatibus nihil sunt odio illo.</p>
+                <button className="bg-blue-800 flex justify-center  items-center font-medium text-white mx-32 border-s-fuchsia-300 rounded-md shadow my-3 px-5 whitespace-nowrap w-max py-2">See More</button>
+            </div>
+            <div className="border border--400 rounded-lg ">
+                <strong className="mx-2"> Cloude computing Expert </strong>
+                <p className="px-5">Lorem ipsum dolor sit amet consectetur<br /> adipisicing  elit. Deserunt nemo, <br /> delectus voluptas quod <br /> omnis, repellat <br />repudiandae ullam asperiores earum <br />sit sint commodi non minus. <br />Fuga necessitatibus nihil sunt odio illo.</p>
+                <button className="bg-blue-800 font-medium text-white mx-32 border-s-fuchsia-300 rounded-md my-2 px-5 whitespace-nowrap w-max py-2">See More</button>
+            </div>
+        </div>
+    )
+}
+export default Cards
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Base from './components/Base.jsx'
-import Grand from './components/Base.jsx'
+import Cards from './components/ExperincesCard.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Footer from './components/footer.jsx'
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-    <Footer />
+    <Cards />
   </StrictMode>
 )
